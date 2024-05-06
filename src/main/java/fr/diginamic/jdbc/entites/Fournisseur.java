446c54cf588f37a51d2406a94943d0610ea68272
lysiandon <don.lysiane@gmail.com>
2024-05-06 19:12:02 +0200

    upd constructor

--- src/main/java/fr/diginamic/jdbc/entites/Fournisseur.java
+++ src/main/java/fr/diginamic/jdbc/entites/Fournisseur.java
@@ -4,6 +4,9 @@ public class Fournisseur {
 	private int id;
 	private String nom;
 	
+	public Fournisseur( String nom) {
+		this.nom = nom;
+	}
 	public Fournisseur(int id, String nom) {
 		this.id = id;
 		this.nom = nom;
@@ -13,9 +16,22 @@ public class Fournisseur {
 	public String toString() {
 		return "Fournisseur id=" + id + ", nom=" + nom + ";";
 	}
-	
-	
-	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
 	
 
 }
